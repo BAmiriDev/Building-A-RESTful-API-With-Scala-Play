abdc06157b5939425c03f3b33add65e48174b6b2
Bilal Amiry <bezyy98@gmail.com>
2024-07-30 10:16:05 +0100

    applicationController methods and tests

---
+++ test/controllers/ApplicationControllerSpec.scala
@@ -0,0 +1,18 @@
+package controllers
+
+import baseSpec.BaseSpecWithApplication
+
+class ApplicationControllerSpec extends BaseSpecWithApplication {
+  val TestApplicationController = new ApplicationController(
+    component
+  )
+
+  "ApplicationController .index()" should {
+
+  }
+
+  "ApplicationController .create()" should {
+
+  }
+
+}
