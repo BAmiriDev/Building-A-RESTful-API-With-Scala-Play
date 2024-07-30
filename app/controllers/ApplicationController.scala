cff018229bc7d5f3b7d42ce5452d42fbe4c9c383
Bilal Amiry <bezyy98@gmail.com>
2024-07-30 14:11:23 +0100

    testing implementation

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -1,17 +1,56 @@
 package controllers
 
+import controllers.models.DataModel
+import play.api.libs.json.{JsError, JsSuccess, JsValue, Json}
 import javax.inject._
 import play.api.mvc._
+import repositories.DataRepository
+import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
-class ApplicationController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
-  def index() = TODO
+class ApplicationController @Inject()(
+                                       val controllerComponents: ControllerComponents,
+                                       dataRepository: DataRepository
+                                     )(implicit ec: ExecutionContext) extends BaseController {
 
-  def create() = TODO
+  def index(): Action[AnyContent] = Action.async { implicit request =>
+    dataRepository.index().map {
+      case Right(items: Seq[DataModel]) => Ok(Json.toJson(items))
+      case Left(error) => Status(error)(Json.toJson("Unable to find any books"))
+    }
+  }
 
-  def read(id: String) = TODO
+  def create(): Action[JsValue] = Action.async(parse.json) { implicit request =>
+    request.body.validate[DataModel] match {
+      case JsSuccess(dataModel, _) =>
+        dataRepository.create(dataModel).map(_ => Created)
+      case JsError(_) => Future(BadRequest)
+    }
+  }
 
-  def update(id: String) = TODO
+  def read(id: String): Action[AnyContent] = Action.async { implicit request =>
+    dataRepository.read(id).map {
+      case Some(dataModel) => Ok(Json.toJson(dataModel))
+      case None => NotFound
+    }
+  }
 
-  def delete(id: String) = TODO
+  def update(id: String): Action[JsValue] = Action.async(parse.json) { implicit request =>
+    request.body.validate[DataModel] match {
+      case JsSuccess(dataModel, _) =>
+        dataRepository.update(id, dataModel).map { updateResult =>
+          if (updateResult.getModifiedCount > 0) Accepted else NotFound
+        }
+      case JsError(_) => Future(BadRequest)
+    }
+  }
+
+  def delete(id: String): Action[AnyContent] = Action.async { implicit request =>
+    dataRepository.delete(id).map { deleteResult =>
+      if (deleteResult.getDeletedCount > 0) Accepted else NotFound
+    }
+  }
+
+
+
 }
--- test/controllers/ApplicationControllerSpec.scala
+++ test/controllers/ApplicationControllerSpec.scala
@@ -1,18 +1,90 @@
 package controllers
 
 import baseSpec.BaseSpecWithApplication
+import play.api.test.Helpers._
+import play.api.test.{FakeRequest, Helpers}
+import repositories.DataRepository
+import controllers.models.DataModel
+import play.api.libs.json.{JsValue, Json}
+import play.api.mvc.Results.Status
+import play.api.mvc.{Result, Results}
+
+import scala.concurrent.Future
 
 class ApplicationControllerSpec extends BaseSpecWithApplication {
+
+  // Inject the necessary components from BaseSpecWithApplication
+  val repository: DataRepository = app.injector.instanceOf[DataRepository]
+
+  // Create the TestApplicationController with the injected components
   val TestApplicationController = new ApplicationController(
-    component
+    component,
+    repository
   )
 
-  "ApplicationController .index()" should {
+  // Define a sample DataModel object for testing
+  private val dataModel: DataModel = DataModel(
+    _id = "abcd",
+    name = "test name",
+    description = "test description",
+    pageCount = 100
+  )
 
+  override def beforeEach(): Unit = {
+    super.beforeEach()
+    await(repository.deleteAll())
   }
 
-  "ApplicationController .create()" should {
-
+  override def afterEach(): Unit = {
+    await(repository.deleteAll())
+    super.afterEach()
   }
 
+  "ApplicationController .index" should {
+    "return a list of DataModels with OK status" in {
+      val result = TestApplicationController.index()(FakeRequest())
+      status(result) shouldBe OK
+      // Additional assertions can be added here to verify the response
+    }
+  }
+
+  "ApplicationController .create" should {
+    "create a book in the database" in {
+      val request: FakeRequest[JsValue] = FakeRequest(POST, "/api").withBody(Json.toJson(dataModel))
+      val createdResult: Future[Result] = TestApplicationController.create()(request)
+
+      status(createdResult) shouldBe CREATED
+      // Additional assertions can be added here to verify the created data
+    }
+  }
+
+  "ApplicationController .read" should {
+
+    "find a book in the database by id" in {
+
+      // Use create method to add a DataModel
+      val createRequest: FakeRequest[JsValue] = FakeRequest(POST, "/api").withBody(Json.toJson(dataModel))
+      val createdResult: Future[Result] = TestApplicationController.create()(createRequest)
+      status(createdResult) shouldBe CREATED  // Checking the status after creation
+
+      // Use read method to retrieve the DataModel by id
+      val readResult: Future[Result] = TestApplicationController.read("abcd")(FakeRequest())
+      status(readResult) shouldBe OK
+      contentAsJson(readResult).as[DataModel] shouldBe dataModel
+    }
+  }
+
+
+
+  "ApplicationController .update()" should {
+    "update an existing DataModel" in {
+      // Implement test logic for the update method
+    }
+  }
+
+  "ApplicationController .delete()" should {
+    "delete a DataModel" in {
+      // Implement test logic for the delete method
+    }
+  }
 }
